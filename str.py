3d38ab138bb8c92c392d0217bb5a50e17ecdaa4b
suye0n6 <acnm86@gmail.com>
2023-12-16 14:14:50 +0900

    str

---
+++ str.py
@@ -0,0 +1,18 @@
+"Hello World"
+'Python is fun'
+"""Life is too short, You need python"""
+'''Life is too short, You need python'''
+
+head = "Python"
+tail = " is fun!"
+head + tail
+
+a = "Life is too short, You need Python"
+a[3]
+
+#문자 개수 세시 -count
+
+a = "hobby"
+a.count('b')
+2
+
